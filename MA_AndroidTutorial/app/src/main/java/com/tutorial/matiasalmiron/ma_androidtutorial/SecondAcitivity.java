ae64e96cc5baa8d285602f5fb9128c39385da6be
Matias Almiron <matias.a@batangamedia.com>
2016-07-14 23:01:54 -0400

    primer ejercicio de service intents 2

--- MA_AndroidTutorial/app/src/main/java/com/tutorial/matiasalmiron/ma_androidtutorial/SecondAcitivity.java
+++ MA_AndroidTutorial/app/src/main/java/com/tutorial/matiasalmiron/ma_androidtutorial/SecondAcitivity.java
@@ -1,15 +1,21 @@
 package com.tutorial.matiasalmiron.ma_androidtutorial;
 
+
+import android.app.Service;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 
-
-public class SecondAcitivity extends AppCompatActivity {
+public class SecondAcitivity extends AppCompatActivity  {
+    Button activityButton;
+    Button startSrv;
+    Button stopSrv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,12 @@ public class SecondAcitivity extends AppCompatActivity {
         setContentView(R.layout.second_acitivity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        activityButton = (Button)findViewById(R.id.activity2Button);
+        startSrv = (Button)findViewById(R.id.activity2StartButton);
+        stopSrv = (Button)findViewById(R.id.activity2StopButton);
+        //startService.setOnClickListener(SecondAcitivity.this);
+        //stopService.setOnClickListener(this);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +38,37 @@ public class SecondAcitivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //event click
+        activityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        startSrv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondAcitivity.this,myService.class);
+                startService(intent);
+            }
+        });
+
+        stopSrv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondAcitivity.this,myService.class);
+                stopService(intent);
+            }
+        });
+
+
+
+
     }
+
+    //private Intent intent;
+
 
 }
---
+++ MA_AndroidTutorial/app/src/main/java/com/tutorial/matiasalmiron/ma_androidtutorial/myService.java
@@ -0,0 +1,42 @@
+package com.tutorial.matiasalmiron.ma_androidtutorial;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+/**
+ * Created by matiasalmiron on 7/14/16.
+ */
+
+public class myService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(this,"Creado el servicio",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this,"Servicio iniciado",Toast.LENGTH_SHORT).show();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this,"Stop Service",Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+}
