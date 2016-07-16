b750d90109f1197256a0b95375ea8d029111729d
Matias Almiron <matias.a@batangamedia.com>
2016-07-16 16:58:04 -0400

    levanto imagenes por intent

--- MA_AndroidTutorial/app/src/main/java/com/tutorial/matiasalmiron/ma_androidtutorial/SecondAcitivity.java
+++ MA_AndroidTutorial/app/src/main/java/com/tutorial/matiasalmiron/ma_androidtutorial/SecondAcitivity.java
@@ -3,15 +3,23 @@ package com.tutorial.matiasalmiron.ma_androidtutorial;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 
 public class SecondAcitivity extends AppCompatActivity  {
@@ -19,8 +27,11 @@ public class SecondAcitivity extends AppCompatActivity  {
     Button startSrv;
     Button stopSrv;
     Button shareSrv;
+    Button pickImage;
     EditText shareText;
+    AppCompatImageView imageView;
 
+    private final int REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +43,7 @@ public class SecondAcitivity extends AppCompatActivity  {
         stopSrv = (Button)findViewById(R.id.activity2StopButton);
         shareSrv = (Button)findViewById(R.id.activitY2ShareButton);
         shareText = (EditText)findViewById(R.id.shareText);
+        pickImage = (Button)findViewById(R.id.activitY2SelectImage);
 
 
 
@@ -75,6 +87,38 @@ public class SecondAcitivity extends AppCompatActivity  {
             }
         });
 
+        pickImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                setResult(RESULT_OK, intent);
+                startActivityForResult(intent,REQUEST_CODE);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Toast.makeText(SecondAcitivity.this,"obtengo la imagen",Toast.LENGTH_SHORT).show();
+            InputStream is = null;
+            try{
+                is = getContentResolver().openInputStream(data.getData());
+                Bitmap bmp = BitmapFactory.decodeStream(is);
+                imageView.setImageBitmap(bmp);
+            }
+            catch (FileNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+
+        }   
     }
 
     public void myClick(View v){
@@ -84,7 +128,6 @@ public class SecondAcitivity extends AppCompatActivity  {
         if(!s.isEmpty()){
 
             intent.putExtra(Intent.EXTRA_TEXT,s);
-            intent.setType("text/plain");
             startActivity(Intent.createChooser(intent,"share via..."));
         }else{
             String sharerUrl = "https://www.facebook.com/sharer/sharer.php?u=" + ulrToShare;
