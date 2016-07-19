2bf46a6a5340538fc640864232b3a31f2f7a21ef
Matias Almiron <matias.a@batangamedia.com>
2016-07-18 22:37:06 -0400

    saco fotos

--- MA_AndroidTutorial/app/src/main/java/com/tutorial/matiasalmiron/ma_androidtutorial/Chapter04Activity.java
+++ MA_AndroidTutorial/app/src/main/java/com/tutorial/matiasalmiron/ma_androidtutorial/Chapter04Activity.java
@@ -4,7 +4,9 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,11 +14,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Chapter04Activity extends AppCompatActivity {
 private final int BLUETOOTH_RESULT_OK = 0;
     Button openWifi;
+    Button photoButton;
+    ImageView imageView;
+    final int IMAGE_CODE = 32;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +41,23 @@ private final int BLUETOOTH_RESULT_OK = 0;
             }
         });
         openWifi = (Button)findViewById(R.id.chapter4OpenWifiButton);
+        photoButton = (Button)findViewById(R.id.chapter4Photo);
+        imageView = (ImageView) findViewById(R.id.chapter4PhotoView);
+
         openWifi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Abro lo settings de l wifi
                 Intent i = new Intent(Settings.ACTION_WIFI_SETTINGS);
                 startActivity(i);
+            }
+        });
+
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(i,IMAGE_CODE);
             }
         });
 
@@ -57,6 +74,11 @@ private final int BLUETOOTH_RESULT_OK = 0;
 
         }else if(requestCode==RESULT_CANCELED){
             Toast.makeText(this,"bluetooth no funca",Toast.LENGTH_SHORT).show();
+        }else if(requestCode==IMAGE_CODE){
+            if(resultCode== RESULT_OK){
+                Bitmap bm = (Bitmap) data.getExtras().get("data");
+                imageView.setImageBitmap(bm);
+            }
         }
     }
 }
