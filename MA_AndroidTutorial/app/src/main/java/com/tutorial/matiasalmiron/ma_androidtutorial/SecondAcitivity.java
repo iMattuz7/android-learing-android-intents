943feea48d59579b250894d164f31bbb1d3b88d8
Matias Almiron <matias.a@batangamedia.com>
2016-07-19 21:56:40 -0400

    usando videos

--- MA_AndroidTutorial/app/src/main/java/com/tutorial/matiasalmiron/ma_androidtutorial/SecondAcitivity.java
+++ MA_AndroidTutorial/app/src/main/java/com/tutorial/matiasalmiron/ma_androidtutorial/SecondAcitivity.java
@@ -1,111 +1,35 @@
 package com.tutorial.matiasalmiron.ma_androidtutorial;
 
 
-import android.app.Service;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatImageView;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import android.widget.VideoView;
 
 
-public class SecondAcitivity extends AppCompatActivity  {
-    Button activityButton;
-    Button startSrv;
-    Button stopSrv;
-    Button shareSrv;
-    Button pickImage;
-    Button goTo3;
-    EditText shareText;
-    AppCompatImageView imageView;
+public class SecondAcitivity extends AppCompatActivity {
+    Button videoButton;
+    VideoView videoView;
+    private final int TAKE_VIDEO = 1;
 
-    private final int REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_acitivity);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        activityButton = (Button)findViewById(R.id.activity2Button);
-        startSrv = (Button)findViewById(R.id.activity2StartButton);
-        stopSrv = (Button)findViewById(R.id.activity2StopButton);
-        shareSrv = (Button)findViewById(R.id.activitY2ShareButton);
-        shareText = (EditText)findViewById(R.id.shareText);
-        pickImage = (Button)findViewById(R.id.activitY2SelectImage);
-        goTo3 = (Button)findViewById(R.id.activity2Go3);
+        videoButton = (Button)findViewById(R.id.activity2Button);
+        videoView = (VideoView) findViewById(R.id.videoView);
 
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        //event click
-        activityButton.setOnClickListener(new View.OnClickListener() {
+        videoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
-            }
-        });
-
-        startSrv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SecondAcitivity.this,myService.class);
-                startService(intent);
-            }
-        });
-
-        stopSrv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SecondAcitivity.this,myService.class);
-                stopService(intent);
-            }
-        });
-        //probando click desde el xml
-        shareSrv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myClick(v);
-            }
-        });
-
-        pickImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                setResult(RESULT_OK, intent);
-                startActivityForResult(intent,REQUEST_CODE);
-            }
-        });
-
-        goTo3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(SecondAcitivity.this,Chapter04Activity.class);
-                startActivity(i);
+                Intent i = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                i.putExtra(MediaStore.EXTRA_VIDEO_QUALITY,1);
+                startActivityForResult(i,TAKE_VIDEO);
             }
         });
 
@@ -115,39 +39,12 @@ public class SecondAcitivity extends AppCompatActivity  {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            Toast.makeText(SecondAcitivity.this,"obtengo la imagen",Toast.LENGTH_SHORT).show();
-            InputStream is = null;
-            try{
-                is = getContentResolver().openInputStream(data.getData());
-                Bitmap bmp = BitmapFactory.decodeStream(is);
-                imageView.setImageBitmap(bmp);
+        if(requestCode==TAKE_VIDEO){
+            if(resultCode == RESULT_OK) {
+                Uri videoUri = data.getData();
+                videoView.setVideoURI(videoUri);
+                videoView.start();
             }
-            catch (FileNotFoundException e)
-            {
-                e.printStackTrace();
-            }
-
-        }   
-    }
-
-    public void myClick(View v){
-        String ulrToShare = "http://99percentinvisible.org/episode/the-mind-of-an-architect/";
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        String s = shareText.getText().toString();
-        if(!s.isEmpty()){
-
-            intent.putExtra(Intent.EXTRA_TEXT,s);
-            startActivity(Intent.createChooser(intent,"share via..."));
-        }else{
-            String sharerUrl = "https://www.facebook.com/sharer/sharer.php?u=" + ulrToShare;
-            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(sharerUrl));
-            startActivity(intent);
         }
-
     }
-
-    //private Intent intent;
-
-
 }
